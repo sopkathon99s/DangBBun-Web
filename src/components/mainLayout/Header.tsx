a9b19a386d927009cdba85139fbd3eaa751e6094
SNUPI <47105088+joohaem@users.noreply.github.com>
2021-11-21 04:32:06 +0900

    feat: 헤더 구현

--- src/components/mainLayout/Header.tsx
+++ src/components/mainLayout/Header.tsx
@@ -1,36 +1,43 @@
-import { Link } from "react-router-dom";
-import { BungaeCard } from "../BungaeCard";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../../assets/logo1.png";
+import { theme } from "../../styles/theme";
 
 export function Header() {
+  const { pathname } = useLocation();
+  console.log(pathname);
   return (
-    <header>
-      <BungaeCard
-        restTime={15}
-        bungaeState="OPEN"
-        title="title"
-        currentCnt={123}
-        minCnt={2}
-        maxCnt={6}
-        date={new Date()}
-        location="loca"
-        detail="detal"
-      />
-      <BungaeCard
-        restTime={15}
-        bungaeState="SUCCESS"
-        title="title"
-        currentCnt={123}
-        minCnt={2}
-        maxCnt={6}
-        date={new Date()}
-        location="loca"
-        detail="detal"
-      />
-      <section>{/* 로고 */}</section>
-      <section>
-        <Link to="/">뻔개 목록 보기</Link>
-        <Link to="/overview">나의 뻔개 목록</Link>
-      </section>
-    </header>
+    <TopBar>
+      <ImgWrapper>
+        <img src={Logo} width="71px" height="23px" alt="logo" />
+      </ImgWrapper>
+      <ul>
+        <Item selected={pathname === "/"}>
+          <Link to="/">뻔개 목록 보기</Link>
+        </Item>
+        <Item selected={pathname === "/overview"}>
+          <Link to="/overview">나의 뻔개 목록</Link>
+        </Item>
+      </ul>
+    </TopBar>
   );
 }
+
+const TopBar = styled.header`
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ImgWrapper = styled.div`
+  margin-left: 1.5rem;
+`;
+
+const Item = styled.li`
+  display: inline-block;
+  margin-right: 1.3rem;
+  color: ${theme.color.red};
+  font-weight: ${(props) => (props.selected ? "bold" : "inherit")};
+`;
